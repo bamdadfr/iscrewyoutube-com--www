32c94fa2df9b7f500b2bbfb71b33dff1e9e3b589
Bamdad Sabbagh <git@bamdadsabbagh.com>
2021-07-13 11:43:17 +0200

    chore(release): rename to release.config.js

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    'plugins': [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        ['@semantic-release/changelog', {
+            'changelogFile': 'CHANGELOG.md',
+        }],
+        '@semantic-release/github',
+        ['@semantic-release/git', {
+            'assets': [
+                'CHANGELOG.md',
+            ],
+            // eslint-disable-next-line no-undef
+            'message': `chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}`,
+        }],
+    ],
+}
